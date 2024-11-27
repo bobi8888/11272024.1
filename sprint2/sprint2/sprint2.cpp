e723f6ef14e7fed7459f554f3be0de14418a5c54
chris <cjaler@gmail.com>
2024-11-27 12:45:24 -0600

    sprint 2 vs files

---
+++ sprint2/sprint2/sprint2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+//#include <SFML/System.hpp>
+//#include <SFML/Graphics.hpp>
+//#include <SFML/Window.hpp>
+//#include <SFML/OpenGL.hpp>
+//#include <SFML/Audio.hpp>
+
+int main()
+{
+std::cout << "AY YO";
+//trying again
+}
