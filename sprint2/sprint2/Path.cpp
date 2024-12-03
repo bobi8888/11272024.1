fde4df0f514a47346eaa57a58fb1b37abe9e7fa9
chris <cjaler@gmail.com>
2024-12-03 12:53:01 -0600

    beginning work on update Path class

--- sprint2/sprint2/Path.cpp
+++ sprint2/sprint2/Path.cpp
@@ -5,12 +5,18 @@ Path::Path(float x, float y, map <int, Point> pointsMap) {
 	Start = sf::Vector2f(x,y);
 	PointsMap = pointsMap;
 }
+void Path::rerollPath() {
+//x should remain 0
+//y should be randomized, take into acount the size of the sprite so that the enemy doesn't partially appear on screen
+//RNG method for #of direction changes
+
+}
 sf::Vector2f Path::getStart() {
 	return Start;
-}
-map<int, Point> Path::getPointsMap() {
-	return PointsMap;
 }
 Point Path::getPoint(int point) {
 	return PointsMap[point];
 }
+//map<int, Point> Path::getPointsMap() {
+//	return PointsMap;
+//}
--- sprint2/sprint2/Path.h
+++ sprint2/sprint2/Path.h
@@ -5,12 +5,16 @@ class Path {
 	private:
 		map <int, Point> PointsMap;
 		sf::Vector2f Start;
+		sf::Vector2f Goal;
+
 	public:
 		Path() {}
 		Path(float x, float y, map <int, Point> point);
+		void rerollPath();
 		sf::Vector2f getStart();
-		map <int, Point> getPointsMap();
 		Point getPoint(int point);
+
+		//map <int, Point> getPointsMap();
 		~Path() {}
 };
 
